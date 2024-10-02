3d9aeedba7f1baf35e693382f255a49ecf7a3cd4
Sang Yub Lee <maruthemax@gmail.com>
2024-10-02 16:17:07 -0700

    Python OOP
    Serial Generator & Random Word

---
+++ python-oo-practice/python-oo-practice/serial.py
@@ -0,0 +1,34 @@
+"""Python serial number generator."""
+
+class SerialGenerator:
+    """Machine to create unique incrementing serial numbers.
+    
+    >>> serial = SerialGenerator(start=100)
+
+    >>> serial.generate()
+    100
+
+    >>> serial.generate()
+    101
+
+    >>> serial.generate()
+    102
+
+    >>> serial.reset()
+
+    >>> serial.generate()
+    100
+    """
+def __init__(self, start=100):
+    """Make a new generator, starting at start."""
+    self.start = self.next = start
+
+def generate(self):
+    """Return next serial."""
+    self.next += 1
+    return self.next - 1
+
+def reset(self):
+    """Reset number to original start."""
+    self.next = self.start
+    
